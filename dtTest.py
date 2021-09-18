29a5dbb5c5563eda5ba942b54b119a1ba788518e
JB Larson <68449234+JBLarson@users.noreply.github.com>
2021-09-18 18:58:26 -0400

    Update dtTest.py

--- dtTest.py
+++ dtTest.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 
+
 import time
 import datetime
 import requests
+import dateparser
 
 
 def convertUnix(epochTime):
@@ -13,19 +15,36 @@ def convertUnix(epochTime):
 def datetimeToUnix(ogDatetime):
 	ogDatetime=datetime.datetime.strptime(ogDatetime, "%Y-%m-%d %H:%M:%S")
 	unixTime = ogDatetime.strftime('%s')
-
 	return unixTime
+
+
+def addDays(ogDatetime, nDays):
+	ogDtTypeStr = str(type(ogDatetime))
+	if 'str' in ogDtTypeStr:	ogDatetime = dateparser.parse(ogDatetime)
+	newDate = ogDatetime + datetime.timedelta(days=nDays)
+	return newDate
+
+
+def subtractDays(ogDatetime, nDays):
+	ogDtTypeStr = str(type(ogDatetime))
+	if 'str' in ogDtTypeStr:	ogDatetime = dateparser.parse(ogDatetime)
+	newDate = ogDatetime - datetime.timedelta(days=nDays)
+	return newDate
+
 
 
 todayFull = datetime.datetime.now()
 todaySplit = str(todayFull).split(".")
 today = todaySplit[0]
+print("Todays date: " + str(today))
 
-print(today)
-
-
-unixToday = datetimeToUnix(today)
-print(unixToday)
+unixTodayTest = datetimeToUnix(today)
+print("\nTodays date in unix time: " + str(unixTodayTest))
 
 
+addDaysTest = addDays(today, 14)
+print("\nTwo weeks from now date: " + str(addDaysTest))
+
+subtractDaysTest = subtractDays(today, 14)
+print("\nTwo weeks ago date: " + str(subtractDaysTest))
 
